55df70e73949b0fbcc9809dc0f4181d8c92a5c51
Shashika Wijayasekera <shashika@wso2telco.com>
2016-05-24 09:52:07 +0530

    PRODEV-382 Move property file reading methods from mediator to dbutils.

---
+++ components/dbutil/src/main/java/com/wso2telco/dbutils/fileutils/FileReader.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright  (c) 2015-2016, WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ *  
+ * WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.wso2telco.dbutils.fileutils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import com.wso2telco.dbutils.util.PropertyFileNames;
+
+public class FileReader {
+
+	private Log LOG = LogFactory.getLog(FileReader.class);
+
+	public Map<String, String> readMediatorConfFile() {
+
+		Map<String, String> mediatorConfMap = new HashMap<String, String>();
+		Properties props = new Properties();
+		FileInputStream fileStream = null;
+
+		String carbonHome = System.getProperty("user.dir");
+		String filePath = carbonHome + "/repository/conf/" + PropertyFileNames.MEDIATOR_CONF_FILE.getFileName();
+
+		try {
+
+			fileStream = new FileInputStream(filePath);
+			props.load(fileStream);
+			mediatorConfMap.put("ussdGatewayEndpoint", props.getProperty("ussdGatewayEndpoint"));
+			mediatorConfMap.put("hubSubsGatewayEndpoint", props.getProperty("hubSubsGatewayEndpoint"));
+			mediatorConfMap.put("hubGateway", props.getProperty("hubGateway"));
+			mediatorConfMap.put("requestRouterUrl", props.getProperty("requestRouterUrl"));
+			mediatorConfMap.put("sendSMSResourceURL", props.getProperty("sendSMSResourceURL"));
+		} catch (FileNotFoundException e) {
+
+			LOG.debug(PropertyFileNames.MEDIATOR_CONF_FILE.getFileName() + " FILE NOT FOUND IN " + filePath, e);
+		} catch (IOException e) {
+
+			LOG.debug("UNABLE TO READ " + PropertyFileNames.MEDIATOR_CONF_FILE.getFileName() + " FILE ", e);
+		} finally {
+
+			try {
+
+				fileStream.close();
+			} catch (IOException e) {
+
+				LOG.debug("UNABLE TO CLOSE " + PropertyFileNames.MEDIATOR_CONF_FILE.getFileName() + " FILE ", e);
+			}
+		}
+
+		return mediatorConfMap;
+	}
+}
