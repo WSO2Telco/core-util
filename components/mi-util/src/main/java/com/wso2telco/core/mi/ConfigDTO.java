c20e27b55a619c5208935d7488051d3a205a43ff
Nuwan Walisundara <nuwan@wso2telco.com>
2016-08-31 15:11:19 +0530

    add new module for mi framework

---
+++ components/mi-util/src/main/java/com/wso2telco/core/mi/ConfigDTO.java
@@ -0,0 +1,54 @@
+package com.wso2telco.core.mi;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+public class ConfigDTO extends Configuration implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8356032431902326005L;
+	
+
+	@JsonProperty
+	private DataSourceFactory database = new DataSourceFactory();
+	
+	/*@JsonProperty
+	private String host;
+	
+	@JsonProperty
+	private int port;*/
+	
+	@JsonProperty
+	private Boolean isMaster;
+	
+	
+	public Boolean getIsMaster() {
+		return isMaster;
+	}
+
+	public Boolean isMaster() {
+		return isMaster;
+	}
+
+	public void setIsMaster(Boolean isMaster) {
+		this.isMaster = isMaster;
+	}
+
+
+	public DataSourceFactory getDataSourceFactory() {
+		return database;
+	}
+
+	@Override
+	public String toString() {
+		return "ConfigDTO [database=" + database + ", isMaster=" + isMaster + "]";
+	}
+	
+
+}
