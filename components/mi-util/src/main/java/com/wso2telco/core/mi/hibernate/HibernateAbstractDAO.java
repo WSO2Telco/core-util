fa271a2b992e25a974a4cb31b233c0af7e2bf566
Shahintha <shahintha@wso2telco.com>
2016-10-28 12:17:03 +0530

    HUBDEV-1039 : Sandbox Release

--- components/mi-util/src/main/java/com/wso2telco/core/mi/hibernate/HibernateAbstractDAO.java
+++ components/mi-util/src/main/java/com/wso2telco/core/mi/hibernate/HibernateAbstractDAO.java
@@ -1,5 +1,7 @@
 package com.wso2telco.core.mi.hibernate;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -18,6 +20,21 @@ public abstract class HibernateAbstractDAO {
 		} catch (Exception ex) {
 			transaction.rollback();
 			LOG.debug("Error Occured While Saving Object. ", ex);
+			throw ex;
+		}
+	}
+	
+	public void saveOrUpdateList(List<Object> objectList) throws Exception {		
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+		    for(Object eachListObj : objectList){
+			session.saveOrUpdate(eachListObj);
+		    }
+		    transaction.commit();
+		} catch (Exception ex) {
+			transaction.rollback();
+			LOG.debug("Error Occured While Saving List of Object. ", ex);
 			throw ex;
 		}
 	}
