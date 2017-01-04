13245ecbe450b93f2eb45570c548b51987628eb5
madushak <poosathecat@gmail.com>
2017-01-04 10:48:25 +0530

    Refactored config util

--- components/config-util/src/main/java/com/wso2telco/core/config/model/Authenticators.java
+++ components/config-util/src/main/java/com/wso2telco/core/config/model/Authenticators.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package com.wso2telco.core.config;
+package com.wso2telco.core.config.model;
 
 import javax.xml.bind.annotation.XmlElement;
 
