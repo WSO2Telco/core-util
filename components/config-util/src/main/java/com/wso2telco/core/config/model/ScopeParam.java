46147f7c1014d8d8783dd735a0194743b7315cb6
lakini <lakini@wso2telco.com>
2017-08-01 15:52:18 +0530

    [IDSDEV-621] Updated ScopeParam class for attribute sharing feature

--- components/config-util/src/main/java/com/wso2telco/core/config/model/ScopeParam.java
+++ components/config-util/src/main/java/com/wso2telco/core/config/model/ScopeParam.java
@@ -1,5 +1,19 @@
+/*******************************************************************************
+ * Copyright  (c) 2015-2017, WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ *
+ * WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.wso2telco.core.config.model;
-
 
 import java.util.List;
 
@@ -32,35 +46,56 @@ public class ScopeParam {
     private boolean isConsentPage;
     private String description;
 
-
     public boolean isConsentPage() {
 		return isConsentPage;
 	}
+
 	public void setConsentPage(boolean isConsentPage) {
 		this.isConsentPage = isConsentPage;
 	}
+
 	public int getScope_id() {
 		return scope_id;
 	}
+
 	public void setScope_id(int scope_id) {
 		this.scope_id = scope_id;
 	}
+
 	public List<LoginHintFormatDetails> getLoginHintFormat() {
         return loginHintFormat;
     }
+
     public boolean isLoginHintMandatory() {
         return isLoginHintMandatory;
     }
+
     public boolean isHeaderMsisdnMandatory() { return isHeaderMsisdnMandatory; }
-    public String getScope() { return scope; }
+
     public msisdnMismatchResultTypes getMsisdnMismatchResult() {
         return msisdnMismatchResult;
     }
+
+    public String getScope() { return scope; }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
     public heFailureResults getHeFailureResult() {
         return heFailureResult;
     }
+
+    public void setHeFailureResult(heFailureResults heFailureResult) {
+        this.heFailureResult = heFailureResult;
+    }
+
     public boolean isTncVisible() {
         return isTncVisible;
+    }
+
+    public void setTncVisible(boolean isTncVisible) {
+        this.isTncVisible = isTncVisible;
     }
 
     public void setLoginHintFormat(List<LoginHintFormatDetails> loginHintFormat) {
@@ -79,22 +114,10 @@ public class ScopeParam {
         this.msisdnMismatchResult = msisdnMismatchResult;
     }
 
-    public void setScope(String scope) {
-        this.scope = scope;
-    }
-
-    public void setHeFailureResult(heFailureResults heFailureResult) {
-        this.heFailureResult = heFailureResult;
-    }
-
-    public void setTncVisible(boolean isTncVisible) {
-        this.isTncVisible = isTncVisible;
-    }
-    
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
