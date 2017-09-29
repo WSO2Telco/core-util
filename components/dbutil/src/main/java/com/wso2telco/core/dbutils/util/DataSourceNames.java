c6b1b11a1a89629d337233dad1832717b4075375
Isuru Dilshan <isurudilsh@gmail.com>
2017-09-29 10:05:44 +0530

    Added mandate db to Datasource name

--- components/dbutil/src/main/java/com/wso2telco/core/dbutils/util/DataSourceNames.java
+++ components/dbutil/src/main/java/com/wso2telco/core/dbutils/util/DataSourceNames.java
@@ -1,16 +1,18 @@
 package com.wso2telco.core.dbutils.util;
 
 public enum DataSourceNames {
-	
-	WSO2AM_DB("jdbc/WSO2AM_DB"),
-	WSO2AM_STATS_DB("jdbc/WSO2AM_STATS_DB"),
-	WSO2UM_DB("jdbc/WSO2UM_DB"),
-	WSO2REG_DB("jdbc/WSO2REG_DB"),
-	WSO2TELCO_DEP_DB("jdbc/WSO2TELCO_DEP_DB");
-	
-	private String jndiName;
-	
-	private DataSourceNames(String jndiName) {
+
+    WSO2AM_DB("jdbc/WSO2AM_DB"),
+    WSO2AM_STATS_DB("jdbc/WSO2AM_STATS_DB"),
+    WSO2UM_DB("jdbc/WSO2UM_DB"),
+    WSO2REG_DB("jdbc/WSO2REG_DB"),
+    WSO2TELCO_DEP_DB("jdbc/WSO2TELCO_DEP_DB"),
+    WSO2TELCO_RATE_DB("jdbc/WSO2TELCO_RATE_DB"),
+    WSO2TELCO_MANDATE_DB("jdbc/WSO2TELCO_MANDATE_DB");
+
+    private String jndiName;
+
+    private DataSourceNames(String jndiName) {
         this.jndiName = jndiName;
     }
 
