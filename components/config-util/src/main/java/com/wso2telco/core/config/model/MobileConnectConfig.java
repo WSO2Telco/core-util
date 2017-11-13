eb5c13aaa6555cfbe904fa42afbf368a63809888
AmilaJ <amilad.jayasekara@gmail.com>
2017-11-13 15:28:36 +0530

    Fix for [IDSDEV-771] Mobile Connect Test Suite Issues

--- components/config-util/src/main/java/com/wso2telco/core/config/model/MobileConnectConfig.java
+++ components/config-util/src/main/java/com/wso2telco/core/config/model/MobileConnectConfig.java
@@ -93,6 +93,17 @@ public class MobileConnectConfig {
 
     private boolean isFederatedDeployment;
 
+    private String wso2APIMDataSourceName;
+
+    @XmlElement(name = "WSO2APIMDataSourceName")
+    public String getWso2APIMDataSourceName() {
+        return wso2APIMDataSourceName;
+    }
+
+    public void setWso2APIMDataSourceName(String wso2APIMDataSourceName) {
+        this.wso2APIMDataSourceName = wso2APIMDataSourceName;
+    }
+
     public boolean isFederatedDeployment() {
         return isFederatedDeployment;
     }
