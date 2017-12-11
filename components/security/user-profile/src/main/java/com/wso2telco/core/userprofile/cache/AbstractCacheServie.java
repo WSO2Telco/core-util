5174cf883d31eb813dc0c512309c628a80f0d24d
shawijayasekera <shashika@wso2telco.com>
2017-12-11 17:19:42 +0530

    CO-6
    Validate JSESSIONID and login the user using the JSESSIONID

--- components/security/user-profile/src/main/java/com/wso2telco/core/userprofile/cache/AbstractCacheServie.java
+++ components/security/user-profile/src/main/java/com/wso2telco/core/userprofile/cache/AbstractCacheServie.java
@@ -1,15 +1,35 @@
+/*******************************************************************************
+ * Copyright  (c) 2015-2016, WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ * <p>
+ * WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.wso2telco.core.userprofile.cache;
 
 import com.wso2telco.core.dbutils.exception.BusinessException;
 import com.wso2telco.core.userprofile.dto.UserProfileDTO;
 
-abstract class AbstractCacheServie  implements UserProfileCachable {
-	
+abstract class AbstractCacheServie implements UserProfileCachable {
+
+	protected abstract UserProfileDTO loadProfile(String key) throws BusinessException;
+
 	@Override
-	final public UserProfileDTO get(String key) throws BusinessException {
-		if(!isExpired(key)) {
-			return get(key);
+	public final UserProfileDTO get(String key) throws BusinessException {
+
+		if (!isExpired(key)) {
+
+			return loadProfile(key);
 		}
-		throw new BusinessException("Session allready expired "+key);
+
+		throw new BusinessException("Session allready expired " + key);
 	}
 }
--- components/security/user-profile/src/main/java/com/wso2telco/core/userprofile/cache/CacheType.java
+++
@@ -1,8 +0,0 @@
-package com.wso2telco.core.userprofile.cache;
-
-public enum CacheType {
-	 Redis ,
-	 Memchached,
-	 Local;
-
-}
