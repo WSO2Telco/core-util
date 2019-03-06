dcec1032b0efad164b77b649304f88711fa1e65a
azhamn <azham@wso2telco.com>
2019-03-06 18:05:39 +0530

    checking debug log is enabled before logging

--- components/security/auth-filter/src/main/java/com/wso2telco/core/authfilter/impl/authorization/BearerAuthenticationFilter.java
+++ components/security/auth-filter/src/main/java/com/wso2telco/core/authfilter/impl/authorization/BearerAuthenticationFilter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016, WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ * <p>
+ * WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.wso2telco.core.authfilter.impl.authorization;
 
 import com.wso2telco.core.authfilter.authentication.BearerAuthenticator;
@@ -61,7 +76,9 @@ public class BearerAuthenticationFilter implements AuthenticationFilter {
             if (tokenInfo.getScopes() != null) {
                 Scope scopeAnnotation = method.getAnnotation(Scope.class);
                 Set<String> allowedScopeSet = new HashSet<>(Arrays.asList(scopeAnnotation.value().split(" ")));
-                log.debug("Access Token: " + tokenInfo.getAccessToken());
+                if (log.isDebugEnabled()) {
+                    log.debug("Access Token: " + tokenInfo.getAccessToken());
+                }
                 isAuthorized = userAuthorizationValidator.isAuthorizedScope(tokenInfo.getScopes(), allowedScopeSet);
 
                 if (isAuthorized) {
