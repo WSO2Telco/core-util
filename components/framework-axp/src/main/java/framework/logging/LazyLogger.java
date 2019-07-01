b7848d794b90e454e25d9d74b35db25b44d3c251
Salinda <salindakarunarathna@gmail.com>
2019-07-01 16:17:49 +0530

    [EXTGW-373] - introduce new module framework + implement lazy logging mechanism + upgrade java version to 1.8

---
+++ components/framework-axp/src/main/java/framework/logging/LazyLogger.java
@@ -0,0 +1,111 @@
+package framework.logging;
+
+import java.util.function.Supplier;
+
+/**
+ * Facade to support application logging.
+ * following log level operation support is available through this class.
+ * <p>
+ * * TRACE
+ * * INFO
+ * * DEBUG
+ * * WARN
+ * * ERROR
+ * * FATAL
+ * <p>
+ *
+ * Create an instance using {@link LazyLoggerFactory#getLogger(Class)}
+ */
+public interface LazyLogger
+{
+
+  /**
+   * Log a message object with the TRACE level.
+   *
+   * @param supplier function to get the message object
+   */
+  void trace(Supplier<?> supplier);
+
+  /**
+   * Log a message object with the TRACE level including the stack trace of the Throwable t passed as parameter
+   *
+   * @param supplier function to get the message object
+   * @param t
+   */
+  void trace(Supplier<?> supplier, Throwable t);
+
+  /**
+   * Log a message object with the INFO level.
+   *
+   * @param supplier function to get the message object
+   */
+  void info(Supplier<?> supplier);
+
+  /**
+   * Log a message object with the INFO level including the stack trace of the Throwable t passed as parameter
+   *
+   * @param supplier function to get the message object
+   * @param t
+   */
+  void info(Supplier<?> supplier, Throwable t);
+
+  /**
+   * Log a message object with the DEBUG level.
+   *
+   * @param supplier function to get the message object
+   */
+  void debug(Supplier<?> supplier);
+
+  /**
+   * Log a message object with the DEBUG level including the stack trace of the Throwable t passed as parameter
+   *
+   * @param supplier function to get the message object
+   * @param t
+   */
+  void debug(Supplier<?> supplier, Throwable t);
+
+  /**
+   * Log a message object with the WARN level.
+   *
+   * @param message
+   */
+  void warn(Object message);
+
+  /**
+   * Log a message object with the WARN level including the stack trace of the Throwable t passed as parameter
+   *
+   * @param message
+   * @param t
+   */
+  void warn(Object message, Throwable t);
+
+  /**
+   * Log a message object with the ERROR level.
+   *
+   * @param message
+   */
+  void error(Object message);
+
+  /**
+   * Log a message object with the ERROR level including the stack trace of the Throwable t passed as parameter
+   *
+   * @param message
+   * @param t
+   */
+  void error(Object message, Throwable t);
+
+  /**
+   * Log a message object with the FATAL level.
+   *
+   * @param message
+   */
+  void fatal(Object message);
+
+  /**
+   * Log a message object with the FATAL level including the stack trace of the Throwable t passed as parameter
+   *
+   * @param message
+   * @param t
+   */
+  void fatal(Object message, Throwable t);
+}
